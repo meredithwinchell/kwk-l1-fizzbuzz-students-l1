94b2f67595a48f4761533de6e38625b5a5d0e8ea
meredithwinchell <meredithwinchell@gmail.com>
2018-06-06 14:24:59 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(int)
  if int % 3 == 0 && int % 5 == 0
- return "FizzBuzz"
+  return "FizzBuzz"
  elsif int % 5 == 0
- return "Buzz"
+   return "Buzz"
  elsif int % 3 == 0
- return "Fizz"
+  return "Fizz"
  end
 end
 
